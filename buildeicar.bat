263716fabf11c7d9aaf46a526ea9444f8d0f4aa6
rich98 <51236157+rich98@users.noreply.github.com>
2019-09-08 19:51:38 +0100

    Update buildeicar.bat

--- buildeicar.bat
+++ buildeicar.bat
@@ -13,5 +13,5 @@ set p9=H*
 echo %p1%%p2%^^%p4%%p5%%p6%%p7%%p8%%p9% >buildeicar.txt
 
 rem This file creates the eicar test string used to test antivirus products.
-rem The set commands are used to breakup the string so not to trigger the antivirus product 
+rem The set commands are used to breakup the string so not to trigger the antivirus product. But when the file is saved the antivirus software is triggered.
 rem The full string can be found here http://2016.eicar.org/85-0-Download.html its near the bottom of that page
