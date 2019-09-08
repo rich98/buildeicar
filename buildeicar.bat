b5bfe4bec483c73c414c7aa29de733a633de4b69
rich98 <51236157+rich98@users.noreply.github.com>
2019-09-08 17:58:01 +0100

    Update buildeicar.bat

--- buildeicar.bat
+++ buildeicar.bat
@@ -12,6 +12,6 @@ set p9=H*
 
 echo %p1%%p2%^^%p4%%p5%%p6%%p7%%p8%%p9% >buildeicar.txt
 
-rem This file creates the eicar test string used to test antivirus products. I'm going to show you an issue with windows defender
+rem This file creates the eicar test string used to test antivirus products.
 rem The set commands are used to breakup the string so not to trigger the antivirus product 
 rem The full string can be found here http://2016.eicar.org/85-0-Download.html its near the bottom of that page
